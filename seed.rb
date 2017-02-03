6badccfc198af7c66b5f1e5cd27d503e498c9b15
Stark Riedesel <starkriedesel@gmail.com>
2017-02-03 12:56:59 -0600

    Sample Windows flag names

--- seed.rb
+++ seed.rb
@@ -10,21 +10,25 @@ def seed!
 
   puts "Seeding tracks"
   Track.all.delete
-  windows = Track.create!({name: 'Windows', color: 'blue', order: 4, diagram: '/windows_network.svg'})
-  linux   = Track.create!({name: 'Linux',   color: 'orange', order: 3})
-  web     = Track.create!({name: 'Web',     color: 'green', order: 2})
-  iot     = Track.create!({name: 'IoT',     color: 'purple', order: 1})
-  crypto  = Track.create!({name: 'Crypto',  color: 'red', order: 5})
+  windows = Track.create!({name: 'Windows', color: 'blue', order: 1, diagram: '/windows_network.svg'})
+  linux   = Track.create!({name: 'Linux',   color: 'orange', order: 2})
+  web     = Track.create!({name: 'Web',     color: 'green', order: 3})
+  iot     = Track.create!({name: 'IoT',     color: 'purple', order: 5})
+  crypto  = Track.create!({name: 'Crypto',  color: 'red', order: 4})
+  other   = Track.create!({name: 'Other',   color: 'red', order: 5})
 
   puts "Seeding flag"
   Flag.all.delete
-  windows10 = Flag.create!({points: 10, track: windows, value: 'flag{Win1}', order: 1})
-  windows20 = Flag.create!({points: 20, track: windows, value: 'flag{Win2}', order: 2})
-  windows30 = Flag.create!({points: 30, track: windows, value: 'flag{Win3}', order: 3})
-  windows30 = Flag.create!({points: 40, track: windows, value: 'flag{Win4}', order: 4})
+  windows10 = Flag.create!({name: 'Access', points: 10, track: windows, value: 'flag{Win1}', order: 1})
+  windows20 = Flag.create!({name: 'Snoop', points: 20, track: windows, value: 'flag{Win2}', order: 2})
+  windows30 = Flag.create!({name: 'Piviot', points: 30, track: windows, value: 'flag{Win3}', order: 3})
+  windows40 = Flag.create!({name: 'Escelate',  points: 40, track: windows, value: 'flag{Win4}', order: 4})
+  windows50 = Flag.create!({name: 'Domain', points: 50, track: windows, value: 'flag{Win5}', order: 5})
   linux10   = Flag.create!({points: 10, track: linux, value: 'flag{Linux1}', order: 1})
   linux20   = Flag.create!({points: 20, track: linux, value: 'flag{Linux2}', order: 2})
   linux30   = Flag.create!({points: 30, track: linux, value: 'flag{Linux3}', order: 3})
+  linux40   = Flag.create!({points: 40, track: linux, value: 'flag{Linux4}', order: 4})
+  linux50   = Flag.create!({points: 50, track: linux, value: 'flag{Linux5}', order: 5})
   web10     = Flag.create!({points: 10, track: web, value: 'flag{Web1}', order: 1})
   web20     = Flag.create!({points: 20, track: web, value: 'flag{Web2}', order: 2})
   web30     = Flag.create!({points: 30, track: web, value: 'flag{Web3}', order: 3})
